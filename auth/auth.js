60af9c9f0eaa8911c60ebf18db78023873ddcab6
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2023-02-25 23:38:56 +0600

    Adding token to registration route

--- auth/auth.js
+++ auth/auth.js
@@ -30,14 +30,25 @@ router.post('/login', (req, res) => {
 router.post('/register', (req, res) => {
   const { login, password, email } = req.body;
 
-  db.query('INSERT INTO users (login, password, email) VALUES (?, ?, ?)', [login, password, email], (error, results, fields) => {
-    if (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Unable to register user' });
+  db.query('SELECT * FROM users WHERE login = ?', [login], (error, results, fields) => {
+    if (results.length > 0) {
+      // User already exists, send error response
+      res.status(409).json({ error: 'User already exists' });
     } else {
-      res.json({ success: true });
+      // User does not exist, create new user
+      db.query('INSERT INTO users (login, password, email) VALUES (?, ?, ?)', [login, password, email], (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        } else {
+          // User created successfully, create and send token
+          const token = jwt.sign({ email }, 'asf', { expiresIn: '1h' });
+          res.json({ token });
+        }
+      });
     }
   });
 });
+
 
 module.exports = router;
